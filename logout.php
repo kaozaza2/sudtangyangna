a0105aa8c430d96174945a42d93bc827c6f859df
Kittisak Phiri <kittisakphiri@gmail.com>
2022-08-06 17:36:37 +0700

    prepare for production

--- logout.php
+++
@@ -1,6 +0,0 @@
-<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-<?php
-session_start() ;
-session_destroy() ;
-echo "<meta http-equiv='refresh' content='0;url=index.php'>" ; 
-?>
