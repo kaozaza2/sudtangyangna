44a654f839b556e73184db20dc15a7a094be28cc
kittisak phiri <kittisakphiri@gmail.com>
2023-02-16 20:51:06 +0700

    initialize 2.x branch

---
+++ database/migrations/2023_02_16_132607_create_products_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('products', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->integer('price');
+            $table->string('image');
+            $table->integer('stock');
+            $table->integer('sell')->default(0);
+            $table->boolean('active')->default(1);
+            $table->unsignedBigInteger('category_id')->nullable();
+            $table->text('detail')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('products');
+    }
+};
